67a263c67cd2f08450c5670b40547f252f4dabf0
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-06 01:04:34 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,12 +1,9 @@
 # don't forget to add: require 'pry'
-require 'pry'
+
 def snake_it_up(string)
   if string[0] == "s"
-
-    ("s" * 10) + string
+    10 * "s" + string
   else
     string
   end
-  binding.pry
-  puts 'yeer'
 end
