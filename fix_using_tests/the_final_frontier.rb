bd93f78a7f19e5b5f16667bb6a00bfb6444475fb
Barbara Bontempo <barbarabontempo94@gmail.com>
2020-06-06 01:35:31 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -3,7 +3,6 @@ require 'pry'
 
 def generate_star_date
   (rand(100000) + 400000) / 10.0
-  binding.pry
 end
 
 
@@ -14,4 +13,6 @@ end
 def engage
   puts state_log(date)
   date = generate_star_date
+  binding.pry
+  puts "hi"
 end
